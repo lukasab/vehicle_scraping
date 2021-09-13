61f2ddcb7af11de57bedae9827148518a93da026
Lukas Alberto Belck <lukas.belck@gmail.com>
2021-09-14 00:50:49 +0200

    Implement Scrapy Item for vehicle brand and scrape models of brands

--- vehicle_scraping/items.py
+++ vehicle_scraping/items.py
@@ -4,9 +4,27 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import TakeFirst, MapCompose
 
 
-class VehicleScrapingItem(scrapy.Item):
+def tranform_int(value):
+    return int(value) if value.isnumeric() else None
+
+
+class VehicleBrandScrapingItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    brand_name = scrapy.Field(
+        input_processor=MapCompose(str.lstrip), output_processor=TakeFirst()
+    )
+    brand_link = scrapy.Field(
+        input_processor=MapCompose(str.lstrip), output_processor=TakeFirst()
+    )
+    brand_image_link = scrapy.Field(
+        input_processor=MapCompose(str.lstrip), output_processor=TakeFirst()
+    )
+    brand_vehicle_in_production = scrapy.Field(
+        input_processor=MapCompose(tranform_int), output_processor=TakeFirst()
+    )
+    brand_vehicle_discontinued = scrapy.Field(
+        input_processor=MapCompose(tranform_int), output_processor=TakeFirst()
+    )
--- vehicle_scraping/spiders/vehicle_spider.py
+++ vehicle_scraping/spiders/vehicle_spider.py
@@ -1,4 +1,6 @@
 import scrapy
+from scrapy.loader import ItemLoader
+from vehicle_scraping.items import VehicleBrandScrapingItem
 
 
 class VehicleSpider(scrapy.Spider):
@@ -8,17 +10,27 @@ class VehicleSpider(scrapy.Spider):
     def parse(self, response):
         brands = response.xpath("//div[@itemscope]")
         for brand in brands:
-            yield {
-                "brand_name": brand.xpath("a/@title").get(),
-                "brand_link": brand.xpath("a/@href").get(),
-                "brand_image_link": brand.xpath("a/img/@src").get(),
-                "brand_vehicle_in_production": brand.xpath(
-                    "following-sibling::div[1]/p[1]/b/text()"
-                ).get(),
-                "brand_vehicle_discontinued": brand.xpath(
-                    "following-sibling::div[1]/p[2]/b/text()"
-                ).get(),
-            }
+            vh_brand = ItemLoader(item=VehicleBrandScrapingItem(), selector=brand)
+            vh_brand.add_xpath("brand_name", "a/@title")
+            vh_brand.add_xpath("brand_link", "a/@href")
+            vh_brand.add_xpath("brand_image_link", "a/img/@src")
+            vh_brand.add_xpath(
+                "brand_vehicle_in_production", "following-sibling::div[1]/p[1]/b/text()"
+            )
+            vh_brand.add_xpath(
+                "brand_vehicle_discontinued", "following-sibling::div[1]/p[2]/b/text()"
+            )
+            vh_brand_item = vh_brand.load_item()
+            yield vh_brand_item
 
-    def parse_brand(self, response):
-        pass
+            yield response.follow(vh_brand_item["brand_link"], self.parse_brand_models)
+
+    def parse_brand_models(self, response):
+        production_models = response.xpath("//div[@class='carmod clearfix ']")
+        for production_model in production_models:
+            yield {"production_model_link": production_model.xpath(".//a/@href").get()}
+        discontinued_models = response.xpath("//div[@class='carmod clearfix disc']")
+        for discontinued_model in discontinued_models:
+            yield {
+                "discontinued_model_link": discontinued_model.xpath(".//a/@href").get()
+            }
