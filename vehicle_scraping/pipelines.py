49ce42b14ec2d00cbbcfb7f9be944bf67c2d0b9c
Lukas Alberto Belck <lukas.belck@gmail.com>
2021-09-15 02:34:31 +0200

    Add feature to export scrapy Items to diferent csv files via pipeline

--- vehicle_scraping/pipelines.py
+++ vehicle_scraping/pipelines.py
@@ -3,11 +3,35 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+from scrapy.exporters import CsvItemExporter
+from vehicle_scraping import items
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+
+def item_type(item):
+    return type(item).__name__
 
 
-class VehicleScrapingPipeline:
+class MultiCSVItemPipeline(object):
+    defined_items = [name for name, _ in items.__dict__.items() if "Item" in name]
+
+    def open_spider(self, spider):
+        self.files = dict(
+            [
+                (name, open("vehicle_scraping/data/" + name + ".csv", "w+b"))
+                for name in self.defined_items
+            ]
+        )
+        self.exporters = dict(
+            [(name, CsvItemExporter(self.files[name])) for name in self.defined_items]
+        )
+        [e.start_exporting() for e in self.exporters.values()]
+
+    def close_spider(self, spider):
+        [e.finish_exporting() for e in self.exporters.values()]
+        [f.close() for f in self.files.values()]
+
     def process_item(self, item, spider):
+        item_name = item_type(item)
+        if item_name in set(self.defined_items):
+            self.exporters[item_name].export_item(item)
         return item
